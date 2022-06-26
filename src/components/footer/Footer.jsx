ed04d21d00ebf4d2f11e9dd489c6c8bd3a10b54f
arifrhm <arif.rahman.rhm@gmail.com>
2022-06-27 00:00:17 +0700

    update github and linkedin footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-import { FaAngellist } from 'react-icons/fa'
+// import { FaAngellist } from 'react-icons/fa'
 import './footer.css';
 
 const Footer = () => {
@@ -17,9 +17,9 @@ const Footer = () => {
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="https://angel.co/u/meri-gogichashvili" target="_blank" rel="noreferrer" ><FaAngellist /></a>
+      <a href="https://www.linkedin.com/in/arif-rahman-a519a6209/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/arifrhm/" target="_blank" rel="noreferrer" ><FaGithub /></a>
+      {/* <a href="https://angel.co/u/meri-gogichashvili" target="_blank" rel="noreferrer" ><FaAngellist /></a> */}
       </div>
       <div className="footer__copyright">
         <small>&copy; ET 2022. All rights reserved.</small>
